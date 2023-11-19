d4625b7ce6d7644a50b5ab94f4ccef23d0f72833
Eric Hanson <5846501+ericphanson@users.noreply.github.com>
2023-11-19 18:14:02 +0100

    fix aliasing bug (#12)
    * update tests
    
    * fix bug by copying when necessary
    
    * bump minimum julia for destructuring
    
    * use ugly syntax to support 1.6 (and 1.5 I guess)
    
    * modernize CI workflow
    
    * update TagBot
    
    * bump to 1.6

--- src/optimized_methods.jl
+++ src/optimized_methods.jl
@@ -22,11 +22,15 @@ function sinkhorn_divergence!(
     f = a.dual_potential
     g = b.dual_potential
     scaled_exp = z -> exp(-z / ρ)
-    unbalanced_sinkhorn!(D, C, a, a, ϵ; kwargs...)
+    f = unbalanced_sinkhorn!(D, C, a, a, ϵ; kwargs...).f
     term_aa = fdot(scaled_exp, a.density, f)
-    unbalanced_sinkhorn!(D, C, b, b, ϵ; kwargs...)
+    g = unbalanced_sinkhorn!(D, C, b, b, ϵ; kwargs...).g
     term_bb = fdot(scaled_exp, b.density, g)
-    unbalanced_sinkhorn!(D, C, a, b, ϵ; kwargs...)
+    # on 1.7+:
+    # (; f, g) = unbalanced_sinkhorn!(D, C, a, b, ϵ; kwargs...)
+    ret = unbalanced_sinkhorn!(D, C, a, b, ϵ; kwargs...)
+    f = ret.f
+    g = ret.g
     term_ab = fdot(scaled_exp, a.density, f) + fdot(scaled_exp, b.density, g)
     return -(ρ + ϵ / 2) * (term_ab - term_aa - term_bb)
 end
